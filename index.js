24d91a8389baa6f8fffac35280f66a42ec1d9e9c
unknown <PugarHuda>
2021-11-20 16:43:40 +0700

    perubahan kedua

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const nama = "Pugar Huda Mantoro";
-let hobi = "main game";
-hobi = "nonton film";
-console.log("saya suka", hobi);
+
+function getNama() {
+    return nama;
+}
+console.log(getNama());
